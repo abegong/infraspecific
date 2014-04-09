185979e42630d316eaf11d337e6036b69f448548
Abe Gong <abegong@gmail.com>
2014-04-08 23:24:03 -0700

    Tweaked filepath in take_pics.py

--- take_pics.py
+++ take_pics.py
@@ -3,5 +3,5 @@ import time
 import datetime
 
 while 1:
-	os.system('ffmpeg -y -f video4linux2 -i /dev/video0 ../pic.jpg')
+	os.system('ffmpeg -y -f video4linux2 -i /dev/video0 /home/pi/temp-pic.jpg')
 	time.sleep(1)
