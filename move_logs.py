ffc4f67ba82d4f762ff10400ec2d0546f318961e
Abe Gong <abegong@gmail.com>
2014-04-08 23:22:58 -0700

    Updated move_logs.py

--- move_logs.py
+++ move_logs.py
@@ -7,7 +7,7 @@ import util
 
 def move_logs(from_path, to_path):
 	now = datetime.datetime.now()
-	exempt_filepath = now.strftime("/%Y/%m/%d")
+	exempt_filepath = "hi"#now.strftime("/%Y/%m/%d")
 
 	G = glob.glob(from_path+'*/*/*')
 
@@ -17,7 +17,12 @@ def move_logs(from_path, to_path):
 			util.build_dirs(to_path, filepath)
 			shutil.move(g, to_path+filepath)
 
-#Mount disk: sudo mount -o uid=pi,gid=pi /dev/sda1 /mnt/2TB_USB
+#Mount disk:
+os.cmd('sudo mount -o uid=pi,gid=pi /dev/sda1 /mnt/2TB_USB')
+
+#Move logs
 move_logs('/home/pi/videolog/', '/mnt/2TB_USB/pi/videolog/')
 move_logs('/home/pi/audiolog/', '/mnt/2TB_USB/pi/audiolog/')
+
 #Unmount disk
+os.cmd('sudo unmount /mnt/2TB_USB')
