5da31558c8a44bdf292729ff67808ed3e8b079cd
Abe Gong <abegong@gmail.com>
2014-04-08 23:55:35 -0700

    Fixed exempt_filepath problem in move_logs.py

--- move_logs.py
+++ move_logs.py
@@ -7,14 +7,14 @@ import util
 
 def move_logs(from_path, to_path):
 	now = datetime.datetime.now()
-	exempt_filepath = "hi"#now.strftime("/%Y/%m/%d")
+	exempt_filepath = now.strftime("%Y/%m/%d")
 
 	G = glob.glob(from_path+'*/*/*/*/*')
 
 	for g in G:
 		filepath = g.split(from_path)[1]
-		
-		if filepath != exempt_filepath:
+
+		if '/'.join(filepath.split('/')[:3]) != exempt_filepath:
 			util.build_dirs(to_path, filepath)
 
 			shutil.move(g, to_path+filepath)
@@ -25,7 +25,7 @@ os.system('sudo mount -o uid=pi,gid=pi /dev/sda1 /mnt/2TB_USB')
 #Move logs
 move_logs('/home/pi/videolog/', '/mnt/2TB_USB/pi/videolog/')
 move_logs('/home/pi/audiolog/', '/mnt/2TB_USB/pi/audiolog/')
-# move_logs('/Users/abe/Desktop/temp2/', '/Users/abe/Desktop/temp/')
+# move_logs('/Users/abe/Desktop/temp/', '/Users/abe/Desktop/temp2/')
 
 #Unmount disk
 os.system('sudo umount /mnt/2TB_USB')
